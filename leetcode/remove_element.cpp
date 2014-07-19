2929f486149855c43f0e106bfc192129d04262fc
Wei Jin <wjin.cn@gmail.com>
2014-07-19 19:24:38 +0800

    fix
    Signed-off-by: Wei Jin <wjin.cn@gmail.com>

--- leetcode/remove_element.cpp
+++ leetcode/remove_element.cpp
@@ -49,6 +49,17 @@ public:
         }
         return i;
     }
+
+    int removeElement3(int A[], int n, int elem)
+    {
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (A[i] != elem) A[idx++] = A[i];
+        }
+
+        return idx;
+    }
 };
 
 int main(int argc, char *argv[])
