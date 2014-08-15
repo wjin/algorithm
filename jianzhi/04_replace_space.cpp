f8d438dd0615ac42bd17000a8fe4b3d2daf2490f
Wei Jin <wjin.cn@gmail.com>
2014-08-15 16:06:27 +0800

    add cc 1.4 description
    Signed-off-by: Wei Jin <wjin.cn@gmail.com>

--- jianzhi/04_replace_space.cpp
+++ jianzhi/04_replace_space.cpp
@@ -40,6 +40,22 @@
 #include <climits>
 #include <utility>
 
+
+/*
+
+1.4
+
+Write a method to replace all spaces in a string with'%20'. You may assume that
+the string has sufficient space at the end of the string to hold the additional
+characters, and that you are given the "true" length of the string.
+
+EXAMPLE
+
+Input: "Mr John Smith
+
+Output: "Mr%20Dohn%20Smith"
+
+*/
 using namespace std;
 
 class Solution
