e5e945ee2af2cd31197e1c46680386b0e868b7df
Wei Jin <wjin.cn@gmail.com>
2014-09-28 18:07:39 +0800

    add bi-bfs
    Signed-off-by: Wei Jin <wjin.cn@gmail.com>

--- misc/bfs/maze.cpp
+++ misc/bfs/maze.cpp
@@ -68,9 +68,13 @@ private:
     struct State {
         int id; // current id
         int fid; // father id
-
-        State(int i = 0, int f = -1) : id(i), fid(f) {}
+        bool tag; // whether visited
+        State(int i = 0, int f = -1, bool t = false) : id(i), fid(f), tag(t) {}
     };
+
+    // each grid stands for a state
+    // so there is a perfect hash solution
+    vector<State> s; // all states
 
     // directions: up, right, bottom, left
     // sometimes, we may have 8 directions
@@ -84,7 +88,7 @@ private:
     }
 
     // extend State according to current State
-    vector<State> extendState(vector<bool> &used, State &cur)
+    vector<State> extendState(State &cur)
     {
         vector<State> ret;
         int x, y, id;
@@ -97,9 +101,9 @@ private:
             id = x * LEN + y;
 
             if (x >= 0 && x < LEN && y >= 0 && y < LEN
-                    && maze[x][y] == 0 && !used[id]) {
-                used[id] = true;
-                ret.push_back(State(id, cur.id));
+                    && maze[x][y] == 0 && !s[id].tag) {
+                s[id].tag = true;
+                ret.push_back(State(id, cur.id, true));
             }
         }
 
@@ -115,40 +119,183 @@ private:
     }
 
 public:
+    Solution() : s(LEN * LEN)
+    {
+    }
+
     void solve()
     {
-        // each grid stands for a state
-        // so there is a perfect hash solution
-        vector<State> s(LEN * LEN); // all states
-        vector<bool> used(LEN * LEN, false); // state tag
-
         queue<State> q;
-        State start(0), end(LEN * LEN - 1);
+        State start(0, -1, true), end(LEN * LEN - 1);
+        s[0] =  start;
         bool found = false;
 
         q.push(start);
-        used[0] = true;
-
-        // bfs
         while (!q.empty() && !found) {
             start = q.front();
             q.pop();
 
-            auto states = extendState(used, start);
+            auto states = extendState(start);
             for (auto e : states) {
-                s[e.id] = e; // store state
-                q.push(e);
-
                 // judge finish
                 if (isTarget(e, end)) {
                     found = true;
+                    s[e.id] = e;
                     break;
                 }
+
+                s[e.id] = e; // store state
+                q.push(e);
             }
         }
 
         if (found) {
             printPath(s, end.id);
+        }
+    }
+};
+
+// bi-directional bfs
+class Solution2
+{
+private:
+    // visit tag
+    // not access, access from start and access from end
+    enum visitTag {
+        NOT, START, END
+    };
+
+    // state definition
+    struct State {
+        int id; // current id
+        int fid; // father id
+        visitTag tag; // visit tag
+        State(int i = 0, int f = -1, visitTag t = NOT) : id(i), fid(f), tag(t) {}
+    };
+
+    // each grid stands for a state
+    // so there is a perfect hash solution
+    vector<State> s; // all states
+
+    // directions: up, right, bottom, left
+    const vector<int> dx = { 0, 1, 0, -1 };
+    const vector<int> dy = { -1, 0, 1, 0 };
+
+    bool isAccessed(State &cur)
+    {
+        // already accessed by another direction?
+        return s[cur.id].tag != NOT;
+    }
+
+    // extend State according to current State
+    // extend even if it was accessed by another direction
+    // so here use s[id].tag != tag)
+    // judge finish in bfs main procedure so that it looks
+    // more clear
+    vector<State> extendState(State &cur, visitTag tag)
+    {
+        vector<State> ret;
+        int x, y, id;
+        for (int i = 0; i < dx.size(); i++) {
+            x = cur.id / LEN;
+            y = cur.id % LEN;
+
+            x += dx[i];
+            y += dy[i];
+            id = x * LEN + y;
+
+            if (x >= 0 && x < LEN && y >= 0 && y < LEN
+                    && maze[x][y] == 0 && s[id].tag != tag) {
+                ret.push_back(State(id, cur.id, tag));
+            }
+        }
+
+        return ret;
+    }
+
+    void printBefore(vector<State> &s, int id)
+    {
+        if (id == -1) return;
+
+        printBefore(s, s[id].fid);
+        cout << '(' << s[id].id/LEN << ", " << s[id].id%LEN << ')' << endl;
+    }
+
+    void printAfter(vector<State> &s, int id)
+    {
+        while (s[id].fid != -1) {
+            cout << '(' << s[id].id/LEN << ", " << s[id].id%LEN << ')' << endl;
+            id = s[id].fid;
+        }
+        cout << '(' << s[id].id/LEN << ", " << s[id].id%LEN << ')' << endl;
+    }
+
+    void printPath(vector<State> &s, int id1, int id2)
+    {
+        printBefore(s, id1);
+        printAfter(s, id2);
+    }
+
+public:
+
+    Solution2() : s(LEN * LEN)
+    {
+    }
+
+    void solve()
+    {
+        queue<State> q, rq;
+        State start(0, -1, START), end(LEN * LEN - 1, -1, END), t;
+        bool found = false;
+        int id1 = -1, id2 = -1;
+
+        q.push(start);
+        s[start.id] = start;
+
+        rq.push(end);
+        s[end.id] = end;
+
+        while (!q.empty() && !rq.empty() && !found) {
+            // search short queue first
+            if (q.size() <= rq.size()) {
+                t = q.front();
+                q.pop();
+
+                auto states = extendState(t, START);
+                for (auto e : states) {
+                    if (isAccessed(e)) {
+                        found = true;
+                        id2 = s[e.id].fid; //
+                        s[e.id] = e; // store state
+
+                        id1 = e.id;
+                        break;
+                    }
+
+                    s[e.id] = e; // store state
+                    q.push(e);
+                }
+            } else {
+                t = rq.front();
+                rq.pop();
+
+                auto states = extendState(t, END);
+                for (auto e : states) {
+                    if (isAccessed(e)) {
+                        found = true;
+                        id1 = e.id;
+                        id2 = e.fid;
+                        break;
+                    }
+
+                    s[e.id] = e; // store state
+                    rq.push(e);
+                }
+            }
+        }
+
+        if (found) {
+            printPath(s, id1, id2);
         }
     }
 };
@@ -167,7 +314,11 @@ int main(int argc, char* argv[])
     }
 
     Solution sol;
+    Solution2 sol2;
+
     sol.solve();
+    cout << endl;
+    sol2.solve();
 
     return 0;
 }
